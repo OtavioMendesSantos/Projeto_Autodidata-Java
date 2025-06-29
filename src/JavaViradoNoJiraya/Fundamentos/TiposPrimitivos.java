5d87532d10f268abd4222760e4d86ed7f477c3f5
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-06-29 19:55:29 -0300

    :black_nib: course: 10 - Tipos primitivos 1

---
+++ src/JavaViradoNoJiraya/Fundamentos/TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package JavaViradoNoJiraya.Fundamentos;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        System.out.println("age: " + age);
+    }
+}
