533e9e67259872baf007c46314ce78728779f9ce
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-06-29 19:39:26 -0300

    :black_nib: course: 0 9 - Comentários

--- src/JavaViradoNoJiraya/Fundamentos/OlaDevDojo.java
+++ src/JavaViradoNoJiraya/Fundamentos/OlaDevDojo.java
@@ -1,6 +1,15 @@
 package JavaViradoNoJiraya.Fundamentos;
 
+/**
+ * Isso é um comentário <b>Javadoc</b>;
+ */
 public class OlaDevDojo {
+    // Comentário de 1 linha
+    /*
+        Um belo comentário
+        de múltiplas linhas
+    */
+
     public static void main(String[] args){
         System.out.println("KA-ME-HA-ME-HA");
     }
